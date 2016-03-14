4f57b38f1ffa2735553175be1e67fdede033d14e
Hitesh MAC <hiteshmac@Hiteshs-MacBook-Pro.local>
2016-03-13 22:51:00 -0400

    equalityComparsion

---
+++ equalityOperator.js
@@ -0,0 +1,8 @@
+console.log(0 == false);   // true
+console.log(0 === false);  // false, because they are of a different type
+console.log(1 == "1" );    // true, automatic type conversion for value only
+console.log(1 === "1" );   // false, because they are of a different type
+console.log(null == undefined); // true
+console.log(null === undefined );// false
+console.log('0' == false); // true
+console.log('0' === false); // false
